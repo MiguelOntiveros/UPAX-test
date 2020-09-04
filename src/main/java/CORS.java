7ad6a169ea69afa02df8728e90bfc2a8dba5e1f3
MiguelOntiveros <MiguelOntiveros@users.noreply.github.com>
2020-09-03 19:09:51 -0500

    make it better

---
+++ src/main/java/CORS.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORS implements ContainerResponseFilter {
+	
+	@Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) throws IOException {
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
